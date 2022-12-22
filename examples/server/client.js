959654dcdf38173a78a24eecfbcd7a372b1a35d8
andreanynthn <and21yonathan@gmail.com>
2022-12-22 13:17:29 +0700

    Update client.js

--- examples/server/client.js
+++ examples/server/client.js
@@ -150,7 +150,7 @@ function start() {
     };
 
     if (document.getElementById('use-video').checked) {
-        var resolution = "640x480";
+        var resolution = "480x360";
         if (resolution) {
             resolution = resolution.split('x');
             constraints.video = {
